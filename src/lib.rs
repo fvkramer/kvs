0d6ff115f75aca2f892796f69763f26706faaa5a
Filipp Kramer <krame490@gmail.com>
2019-07-10 20:24:49 -0700

    get tests to compile

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,25 @@
+
 #[allow(non_snake_case)]
 pub mod KvStore {
+    pub struct Store {
 
+    }
+
+    pub fn new() -> Store {
+        Store { }
+    }
+
+    impl Store {
+        pub fn set(&self, _key: String, _value: String)  {
+            panic!(); 
+        }
+
+        pub fn get(&self, _key: String) -> Option<String> {
+            panic!();
+        }
+
+        pub fn remove(&self, _key: String) {
+            panic!();
+        }
+    }
 }
