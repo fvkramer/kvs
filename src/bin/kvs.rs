31cd7f580f489b4b6f0b6b1cba25a60011311bff
Filipp Kramer <krame490@gmail.com>
2019-07-14 13:39:12 -0700

    refactor clap commands and pass cli tests

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,13 +1,41 @@
 extern crate clap;
 use clap::{Arg, App, SubCommand}; 
+use std::process::exit;
 
+#[allow(unused_imports)]
 fn main() {
     let matches = App::new("kvs rust storage")
                         .version(env!("CARGO_PKG_VERSION"))
                         .author(env!("CARGO_PKG_AUTHORS"))
                         .about("Parses CLI args for test db")
-                        .arg(Arg::with_name("get"))
-                        .arg(Arg::with_name("set"))
-                        .arg(Arg::with_name("remove"))
+                        .subcommand(
+                            SubCommand::with_name("get")
+                                .arg(Arg::with_name("key"))
+                        )
+                        .subcommand(
+                            SubCommand::with_name("set")
+                                .arg(Arg::with_name("key"))
+                                .arg(Arg::with_name("value"))
+                        )
+                        .subcommand(
+                            SubCommand::with_name("rm")
+                                .arg(Arg::with_name("key"))
+                        )
                         .get_matches();
+
+    match matches.subcommand() {
+        ("get", Some(get_matches)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        ("set", Some(set_matches)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        ("rm", Some(rm_matches)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        _   => unreachable!(),
+    }
 }
