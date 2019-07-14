b6b3312e51717dc2223055b06eb5dbb62620e2ef
Filipp Kramer <krame490@gmail.com>
2019-07-13 19:55:46 -0700

    parsing implementation in main binary

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,13 @@
+extern crate clap;
+use clap::{Arg, App, SubCommand}; 
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("kvs rust storage")
+                        .version(env!("CARGO_PKG_VERSION"))
+                        .author(env!("CARGO_PKG_AUTHORS"))
+                        .about("Parses CLI args for test db")
+                        .arg(Arg::with_name("get"))
+                        .arg(Arg::with_name("set"))
+                        .arg(Arg::with_name("remove"))
+                        .get_matches();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-
 #[allow(non_snake_case)]
 pub mod KvStore {
     pub struct Store {
